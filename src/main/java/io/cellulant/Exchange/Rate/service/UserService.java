cbbca62a995b534cad73b6353fb62f932b25867f
allanlimo02 <allanlimo02@gmail.com>
2022-12-01 20:36:28 +0300

    Fixed createuser route

--- src/main/java/io/cellulant/Exchange/Rate/service/UserService.java
+++ src/main/java/io/cellulant/Exchange/Rate/service/UserService.java
@@ -1,7 +1,7 @@
 package io.cellulant.Exchange.Rate.service;
 
 import io.cellulant.Exchange.Rate.dto.UserDetails;
-import io.cellulant.Exchange.Rate.repositories.User_repository;
+import io.cellulant.Exchange.Rate.interfaces.User_repository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -14,14 +14,14 @@ public class UserService {
 
 
     //add new User
-    public UserDetails newUser(UserDetails userDetails){
+    public List <UserDetails> newUser(UserDetails userDetails){
         try{
             user_repository.save(userDetails);
         }catch (Exception e){
             System.out.println(e);
         }
 
-        return user_repository.save(userDetails);
+        return user_repository.findAll();
     }
     //add multiple new users
     public List <UserDetails> addUsers(List <UserDetails> userDetails){
