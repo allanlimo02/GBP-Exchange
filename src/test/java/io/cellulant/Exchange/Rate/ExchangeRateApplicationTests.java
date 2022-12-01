0bd7cc35931e33d87b50748bac2c54b4674954ed
allanlimo02 <allanlimo02@gmail.com>
2022-12-01 14:13:23 +0300

    Updated postmapping

---
+++ src/test/java/io/cellulant/Exchange/Rate/ExchangeRateApplicationTests.java
@@ -0,0 +1,13 @@
+package io.cellulant.Exchange.Rate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExchangeRateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
