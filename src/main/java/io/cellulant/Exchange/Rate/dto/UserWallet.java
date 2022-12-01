7e1ab05f31d3da076517598de557b1c5d4598f88
allanlimo02 <allanlimo02@gmail.com>
2022-12-01 14:20:29 +0300

    Updated postmapping

---
+++ src/main/java/io/cellulant/Exchange/Rate/dto/UserWallet.java
@@ -0,0 +1,22 @@
+package io.cellulant.Exchange.Rate.dto;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.Fetch;
+
+@Data
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table
+@Entity
+public class UserWallet {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int walletId;
+    @Column(name = "user_id")
+    private int userId;
+
+
+}
